4cfdf7c79a2273c3bcb245e342c849deebc0f97b
YessineLajmi <yassinlajmi@hotmail.com>
2023-05-03 22:09:55 +0100

    last one

---
+++ scriptt.js
@@ -0,0 +1,10 @@
+const friends = ["yassine", "ahmed", "wisssem"];
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+
+friends.push("omi");
+console.log(friends);
+
+console.log(friends.indexOf("omi"));
